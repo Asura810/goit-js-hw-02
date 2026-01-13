ba4887848755737f837a714d028624b392518232
Asura <asurahurbanova@gmail.com>
2026-01-13 15:39:31 +0100

    fix js1 and js4

--- js/task-4.js
+++ js/task-4.js
@@ -3,12 +3,17 @@ function getShippingCost(country) {
   switch (country) {
     case 'China':
       price = 100;
+      break;
     case 'Chile':
       price = 250;
+      break;
     case 'Australia':
       price = 170;
+      break;
     case 'Jamaica':
       price = 120;
+      break;
+
     default:
       return 'Sorry, there is no delivery to your country';
   }
